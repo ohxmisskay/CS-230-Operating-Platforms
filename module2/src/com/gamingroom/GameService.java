23012b870676bde269059518512e68d0fbf9b446
ohxmisskay <130874058+ohxmisskay@users.noreply.github.com>
2024-09-18 12:39:17 -0700

    Module 2 Assignment

---
+++ module2/src/com/gamingroom/GameService.java
@@ -0,0 +1,140 @@
+package com.gamingroom;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A singleton service for the game engine
+ * 
+ * @author coce@snhu.edu
+ */
+public class GameService {
+
+	/**
+	 * A list of the active games
+	 */
+	private static List<Game> games = new ArrayList<Game>();
+
+	/*
+	 * Holds the next game identifier
+	 */
+	private static long nextGameId = 1;
+
+	
+	/*
+	 * Only one local instance of class, implements the singleton design pattern
+	 * simply by making the singleton class constructor private
+	 */
+	private static GameService instance = new GameService();
+	
+	
+	/*
+	 * Private constructor which will prevent attempts to instantiate 
+	 * more than one instance of the GameService object. 
+	 */
+	private void GameService() {
+	}
+	
+	/*
+	 * Public accessor that allows outside classes 
+	 * to access singleton design pattern
+	 */
+	public static GameService getInstance() {
+		return instance;
+	}
+
+	/**
+	 * Construct a new game instance
+	 * 
+	 * @param name the unique name of the game
+	 * @return the game instance (new or existing)
+	 */
+	public Game addGame(String name) {
+
+		// a local game instance
+		Game game = null;
+
+		// Use iterator to look for existing game with same name
+		// if found, simply return the existing instance
+		for (Game currentGame : games) {
+			if (currentGame.getName().equals(name)) {
+			return currentGame;
+			}
+		}
+		
+
+		// if not found, make a new game instance and add to list of games
+		if (game == null) {
+			game = new Game(nextGameId++, name);
+			games.add(game);
+		}
+
+		// return the new/existing game instance to the caller
+		return game;
+	}
+
+	/**
+	 * Returns the game instance at the specified index.
+	 * <p>
+	 * Scope is package/local for testing purposes.
+	 * </p>
+	 * @param index index position in the list to return
+	 * @return requested game instance
+	 */
+	Game getGame(int index) {
+		return games.get(index);
+	}
+	
+	/**
+	 * Returns the game instance with the specified id.
+	 * 
+	 * @param id unique identifier of game to search for
+	 * @return requested game instance
+	 */
+	public Game getGame(long id) {
+
+		// a local game instance
+		Game game = null;
+
+		// Use iterator to look for existing game with same id
+		// If current game equates to ID, assigns instance to the local variable
+		for (Game currentGame : games) {
+			if (currentGame.getId() == id){
+			game = currentGame;
+			}
+		}
+
+		return game;
+	}
+
+	/**
+	 * Returns the game instance with the specified name.
+	 * 
+	 * @param name unique name of game to search for
+	 * @return requested game instance
+	 */
+	public Game getGame(String name) {
+
+		// a local game instance
+		Game game = null;
+
+		// Use iterator to look for existing game with same name
+		// If same name is found, assign instance to the local variable
+		for (Game currentGame : games) {
+			if (currentGame.getName().equals(name)){
+			game = currentGame;
+			}
+		}
+
+		return game;
+	}
+
+	/**
+	 * Returns the number of games currently active
+	 * 
+	 * @return the number of games currently active
+	 */
+	public int getGameCount() {
+		return games.size();
+	}
+}
